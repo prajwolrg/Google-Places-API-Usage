4ef0f51b8e9be8f1e6bff240dd3036d1f89782cd
Madhav Aryal <074bct520.madhav@pcampus.edu.np>
2020-09-02 22:52:02 +0545

    final from gyawali and me

---
+++ README.md
@@ -0,0 +1,5 @@
+## use `yarn` or `npm install` to install dependencies
+
+## set `PLACES_API_KEY` environment variablle with your api key
+
+## use `node index.js` to run
---
+++ index.js
@@ -0,0 +1,124 @@
+const axios = require("axios");
+const fs = require("fs");
+
+// filename = "restaurants.json";
+filename = "party_palaces.json";
+let allRestaurants = JSON.parse(fs.readFileSync(filename));
+const r_earth = 6378000;
+
+const data_radius = 3000;
+
+centerPoints = [];
+
+function getPoints(lat, long, dy, dx, area) {
+  centerPoints.push(`${lat},${long}`);
+  for (let i = 0; i < area; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (i % 2 == 0) {
+        lat = lat - (dy / r_earth) * (180 / Math.PI); //left
+      } else {
+        lat = lat + (dy / r_earth) * (180 / Math.PI);
+      }
+      centerPoints.push(`${lat},${long}`);
+    }
+
+    for (let j = 0; j <= i; j++) {
+      if (i % 2 == 0) {
+        long =
+          long +
+          ((dx / r_earth) * (180 / Math.PI)) / Math.cos((lat * Math.PI) / 180); //up
+      } else {
+        long =
+          long -
+          ((dx / r_earth) * (180 / Math.PI)) / Math.cos((lat * Math.PI) / 180);
+      }
+      centerPoints.push(`${lat},${long}`);
+      // centerPoints.push({ lat, long });
+    }
+  }
+  console.log(centerPoints);
+}
+
+async function getData(currLocation) {
+  console.log("getting data for ", currLocation);
+  let response = await axios.get(
+    "https://maps.googleaPIs.com/maps/api/place/nearbysearch/json",
+    {
+      params: {
+        location: currLocation, // run every time by changing index. run using 'node index.js'
+        radius: data_radius,
+        type: "restaurant",
+        keyword: "party",
+        key: process.env.PLACES_API_KEY,
+      },
+    }
+  );
+  console.log(response.data.status, response.data.results.length);
+  allRestaurants.push(
+    ...response.data.results.map((el) => {
+      return { name: el.name, location: el.geometry.location };
+    })
+  );
+
+  if (response.data.next_page_token) {
+    pagetoken = response.data.next_page_token;
+  } else {
+    fs.writeFileSync(
+      filename,
+      JSON.stringify(
+        allRestaurants.filter(
+          (restaurant, index, self) =>
+            index === self.findIndex((t) => t.name === restaurant.name)
+        )
+      )
+    );
+    return;
+  }
+
+  let interval = setInterval(async () => {
+    response = await axios.get(
+      "https://maps.googleaPIs.com/maps/api/place/nearbysearch/json",
+      {
+        params: {
+          pagetoken,
+          key: PLACES_API_KEY,
+        },
+      }
+    );
+    allRestaurants.push(
+      ...response.data.results.map((el) => {
+        return { name: el.name, location: el.geometry.location };
+      })
+    );
+    if (response.data.next_page_token) {
+      pagetoken = response.data.next_page_token;
+    } else {
+      fs.writeFileSync(
+        "party_palaces.json",
+        JSON.stringify(
+          allRestaurants.filter(
+            (restaurant, index, self) =>
+              index === self.findIndex((t) => t.name === restaurant.name)
+          )
+        )
+      );
+      clearInterval(interval);
+    }
+  }, 7000);
+}
+
+async function getMultipleData() {
+  getPoints(27.7172, 85.324, data_radius, data_radius, 5);
+  console.log(centerPoints.length);
+
+  let i = 0;
+  const outsideInterval = setInterval(async () => {
+    await getData(centerPoints[i]);
+    i++;
+    if (i == centerPoints.length) {
+      clearInterval(outsideInterval);
+    }
+  }, 5000);
+}
+
+getMultipleData();
